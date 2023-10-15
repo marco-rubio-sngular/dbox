4650bd5d05d757883cad29198b790445ad36f9a3
Marco Antonio <marco.rubio@sngular.com>
2023-10-15 11:00:04 +0200

    - limpiamos el test del VO LimitedString'

--- backend/api/components/test/Shared/Domain/ValueObject/LimitedString.spec.ts
+++ backend/api/components/test/Shared/Domain/ValueObject/LimitedString.spec.ts
@@ -5,31 +5,29 @@ class LimitedStringImp extends LimitedString {
     throw new Error(this.value);
   }
 }
+
+const MINIMUM: number = 5;
+const MAXIMUM: number = 50;
+
 describe("Limited string value object", () => {
-  it("should throw a child exception if char length its wrong, 5 - 50", () => {
+  it(`should throw a child exception if char length its wrong, ${MINIMUM} - ${MAXIMUM}`, () => {
     expect(() => {
-      new LimitedStringImp("", 5, 50);
+      new LimitedStringImp("", MINIMUM, MAXIMUM);
     }).toThrow();
     expect(() => {
-      new LimitedStringImp("a".repeat(5 - 1), 5, 50);
+      new LimitedStringImp("a".repeat(MINIMUM - 1), MINIMUM, MAXIMUM);
     }).toThrow();
     expect(() => {
-      new LimitedStringImp("a".repeat(50 + 1), 5, 50);
-    }).toThrow();
-    expect(() => {
-      new LimitedStringImp("a".repeat(3), 5, 50);
+      new LimitedStringImp("a".repeat(MAXIMUM + 1), MINIMUM, MAXIMUM);
     }).toThrow();
   });
 
-  it("should not throw a child exception if char length its between 5 - 50", () => {
+  it(`should not throw a child exception if char length its between ${MINIMUM} - ${MAXIMUM}`, () => {
     expect(() => {
-      new LimitedStringImp("a".repeat(5), 5, 50);
+      new LimitedStringImp("a".repeat(MINIMUM), MINIMUM, MAXIMUM);
     }).not.toThrow();
     expect(() => {
-      new LimitedStringImp("a".repeat(50), 5, 50);
-    }).not.toThrow();
-    expect(() => {
-      new LimitedStringImp("a".repeat(32), 5, 50);
+      new LimitedStringImp("a".repeat(MAXIMUM), MINIMUM, MAXIMUM);
     }).not.toThrow();
   });
 });
