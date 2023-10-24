87b3c3f1b523e26516a30dcec47ec594a219ae54
Marco Antonio <marco.rubio@sngular.com>
2023-10-24 18:28:49 +0200

    - faq con id y servicio hace ya uso  del repo para la creacion el servicio esta listo

--- backend/api/src/Context/Support/Faq/Application/Create/FaqCreateService.ts
+++ backend/api/src/Context/Support/Faq/Application/Create/FaqCreateService.ts
@@ -4,17 +4,22 @@ import Title from '../../../../Shared/Domain/ValueObject/Title';
 import Faq from '../../Domain/Model/Faq';
 import Solution from '../../Domain/ValueObject/Solution';
 import FaqCreateRequest from './FaqCreateRequest';
+
 class FaqCreateService {
     constructor(private readonly repository: FaqRepository) {}
 
     execute(request: FaqCreateRequest): void {
-        const faq: Faq = Faq.create(
+        const faq: Faq = this.createFaqFromRequest(request);
+
+        this.repository.create(faq);
+    }
+
+    private createFaqFromRequest(request: FaqCreateRequest): Faq {
+        return Faq.create(
             new Id(request.id),
             new Title(request.title),
             new Solution(request.solution)
         );
-
-        this.repository.create(faq);
     }
 }
 
